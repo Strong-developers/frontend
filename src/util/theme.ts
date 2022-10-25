b6cd71ed934a8474da941b2cf86c4cf8ce3729ef
foxmon <jjockrod@naver.com>
2022-10-26 00:59:32 +0900

    Feat: Custom theme

---
+++ src/util/theme.ts
@@ -0,0 +1,14 @@
+const Theme = {
+  fontSize: {
+    small: "0.875rem",
+    default: "1rem",
+    littleBig: "2rem",
+    big: "2.5rem",
+  },
+  colors: {
+    black: "#ffffff",
+    white: "#000000",
+  },
+};
+
+export default Theme;
