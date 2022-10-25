4ee4634981a0cc38c54895d5fa125718554e4f49
origin1508 <origin1508@gmail.com>
2022-10-26 02:05:00 +0900

    Feat: login, register 페이지 추가

--- src/router/CustomRouter.tsx
+++ src/router/CustomRouter.tsx
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const MainComponentPage = React.lazy(() => import("../pages/Main"));
+const LoginComponentPage = React.lazy(() => import("../pages/Login"));
+const RegisterComponentPage = React.lazy(() => import("../pages/Register"));
 
 const CustomRouter = () => {
   return (
@@ -9,6 +11,8 @@ const CustomRouter = () => {
       <Suspense fallback={<div>Loading</div>} />
       <Routes>
         <Route path="/" element={<MainComponentPage />} />
+        <Route path="/login" element={<LoginComponentPage />} />
+        <Route path="/register" element={<RegisterComponentPage />} />
       </Routes>
     </React.Fragment>
   );
