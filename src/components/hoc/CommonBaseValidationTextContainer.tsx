3deba837bc1154f2a50a9d31ce08732978463c5b
origin1508 <origin1508@gmail.com>
2022-10-26 02:28:49 +0900

    Fix: validation 문구 글자 크기 수정

--- src/components/hoc/CommonBaseValidationTextContainer.tsx
+++ src/components/hoc/CommonBaseValidationTextContainer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { CommonComponentType } from "../../types/commonComponentType";
+import theme from "../../util/theme";
 
 const CommonBaseValidationTextContainer = ({
   children,
@@ -17,4 +18,5 @@ export default CommonBaseValidationTextContainer;
 const CommonBaseValidationTextContainerStyle = styled.div`
   color: red;
   width: 100%;
+  font-size: ${theme.fontSize.smaller};
 `;
