23773d27eb29fd788fa143bba8b4fb537cfd9ebc
origin1508 <origin1508@gmail.com>
2022-10-27 21:27:39 +0900

    Fix: CommonLabel 추가

--- src/assets/styles/commonStyle.ts
+++ src/assets/styles/commonStyle.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
+import Theme from "../../util/theme";
 
 export const CommonInput = styled.input`
   padding: 5px 12px 5px 12px;
   border: 1px solid rgba(0, 0, 0, 0.3);
   border-radius: 6px;
   box-sizing: border-box;
+  font-size: 16px;
 
   &:focus {
-    border: 1px solid black;
-    outline: none;
+    outline: 1px solid black;
   }
 `;
 
@@ -16,4 +17,12 @@ export const CommonButton = styled.button`
   border: none;
   border-radius: 6px;
   cursor: pointer;
+`;
+
+export const CommonLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 14px;
+  margin-bottom: 3px;
 `;
