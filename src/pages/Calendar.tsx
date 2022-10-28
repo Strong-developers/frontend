88c6d9410b3b7b0e4b047f6e66dede4f739b78f4
foxmon <jjockrod@naver.com>
2022-10-28 17:33:07 +0900

    Fix: Calendar refactor

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,35 +1,17 @@
-import React, { useState, useMemo } from "react";
+import React from "react";
+import useCalendar from "../hooks/useCalendar";
 import CalendarHeader from "../components/calendar/CalendarHeader";
 import CalendarDay from "../components/calendar/CalendarDay";
 import CalendarDayNumber from "../components/calendar/CalendarDayNumber";
-import { startYear, startMonth, daysInMonth } from "../util/dateUtil";
 
 const Calendar = () => {
-  const [curYear, setCurYear] = useState<number>(startYear());
-  const [curMonth, setCurMonth] = useState<number>(startMonth());
-
-  const curDays = useMemo(
-    () => daysInMonth(curYear, curMonth),
-    [curYear, curMonth]
-  );
-
-  const handlePrevMonthButtonClick = () => {
-    if (curMonth - 1 < 1) {
-      setCurYear((prev) => prev - 1);
-      setCurMonth(12);
-    } else {
-      setCurMonth((prev) => prev - 1);
-    }
-  };
-
-  const handleNextMonthButtonClick = () => {
-    if (curMonth + 1 > 12) {
-      setCurYear((prev) => prev + 1);
-      setCurMonth(1);
-    } else {
-      setCurMonth((prev) => prev + 1);
-    }
-  };
+  const {
+    curYear,
+    curMonth,
+    curDays,
+    handleNextMonthButtonClick,
+    handlePrevMonthButtonClick,
+  } = useCalendar();
 
   return (
     <React.Fragment>
