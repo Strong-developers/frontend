ad8994b282909d1624d1e6619c35a961d5924647
foxmon <jjockrod@naver.com>
2022-10-29 20:12:18 +0900

    Fix: AuthRegister

--- src/components/auth/AuthRegister.tsx
+++ src/components/auth/AuthRegister.tsx
@@ -1,100 +1,16 @@
-import React from "react";
 import styled from "styled-components";
-import { UseFormRegister, FieldErrorsImpl } from "react-hook-form";
+import AuthCommonHeader from "./common/AuthCommonHeader";
 import AuthMemberSelector from "./AuthMemberSelector";
-import { AuthFormType } from "../../types/auth/authType";
-import CommonBaseInputContainer from "../hoc/CommonBaseInputContainer";
-import CommonBaseValidationTextContainer from "../hoc/CommonBaseValidationTextContainer";
-import {
-  CommonInput,
-  ThemeButton,
-  CommonLabel,
-} from "../../assets/styles/commonStyle";
+import AuthRegisterFormComponent from "./form/AuthRegisterFormComponent";
+import { AuthFormPropsType } from "../../types/auth/authType";
+import Theme from "../../util/theme";
 
-interface AuthRegisterPropsType {
-  register: UseFormRegister<AuthFormType>;
-  errors: Partial<FieldErrorsImpl<AuthFormType>>;
-}
-
-const AuthRegister = ({ register, errors }: AuthRegisterPropsType) => {
+const AuthRegister = ({ register, errors }: AuthFormPropsType) => {
   return (
     <AuthRegisterFormContainer>
+      <AuthCommonHeader />
       <AuthMemberSelector />
-      <AuthRegisterForm>
-        <CommonBaseInputContainer>
-          <AuthRegisterLabel>
-            이메일
-            <AuthRegisterInput
-              {...register("email", {
-                required: true,
-                pattern: /^\S+@\S+$/i,
-              })}
-              type="text"
-            />
-          </AuthRegisterLabel>
-          {errors.email && (
-            <CommonBaseValidationTextContainer>
-              올바른 이메일을 입력해주세요.
-            </CommonBaseValidationTextContainer>
-          )}
-        </CommonBaseInputContainer>
-        <CommonBaseInputContainer>
-          <AuthRegisterLabel>
-            닉네임
-            <AuthRegisterInput
-              {...register("nickname", {
-                required: true,
-                minLength: 4,
-              })}
-              type="text"
-            />
-          </AuthRegisterLabel>
-          {errors.nickname && (
-            <CommonBaseValidationTextContainer>
-              닉네임을 4글자 이상 사용해주세요.
-            </CommonBaseValidationTextContainer>
-          )}
-        </CommonBaseInputContainer>
-        <CommonBaseInputContainer>
-          <AuthRegisterLabel>
-            비밀번호
-            <AuthRegisterInput
-              {...register("password", {
-                required: true,
-                minLength: 8,
-                maxLength: 15,
-                pattern: /^.(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/,
-              })}
-              type="password"
-            />
-          </AuthRegisterLabel>
-          {errors.password && (
-            <CommonBaseValidationTextContainer>
-              8~15자의 영문 소문자, 숫자, 특수문자를 조합해주세요.
-            </CommonBaseValidationTextContainer>
-          )}
-        </CommonBaseInputContainer>
-        <CommonBaseInputContainer>
-          <AuthRegisterLabel>
-            비밀번호확인
-            <AuthRegisterInput
-              {...register("confirmPassword", {
-                required: true,
-              })}
-              type="password"
-            />
-          </AuthRegisterLabel>
-          <CommonBaseValidationTextContainer>
-            비밀번호가 일치하지 않습니다.
-          </CommonBaseValidationTextContainer>
-        </CommonBaseInputContainer>
-        <AuthRegisterButtonContainer>
-          <ThemeButton>회원가입</ThemeButton>
-          <span>
-            이미 회원이신가요?<a href="/login">로그인</a>
-          </span>
-        </AuthRegisterButtonContainer>
-      </AuthRegisterForm>
+      <AuthRegisterFormComponent register={register} errors={errors} />
     </AuthRegisterFormContainer>
   );
 };
@@ -102,26 +18,11 @@ const AuthRegister = ({ register, errors }: AuthRegisterPropsType) => {
 export default AuthRegister;
 
 const AuthRegisterFormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  margin-top: 150px;
+  width: 30rem;
+  margin: 0 auto;
+  margin-top: 1rem;
+  padding: 1rem;
   font-weight: bold;
-`;
-
-const AuthRegisterForm = styled.form`
-  width: auto;
-`;
-
-const AuthRegisterLabel = styled(CommonLabel)``;
-
-const AuthRegisterInput = styled(CommonInput)`
-  margin-top: 5px;
-  height: 40px;
-  width: 400px;
-`;
-
-const AuthRegisterButtonContainer = styled.div`
-  margin-top: 30px;
+  border: 1px solid ${Theme.colors.black};
+  border-radius: 8px;
 `;
