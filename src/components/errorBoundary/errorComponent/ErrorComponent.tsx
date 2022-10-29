76b81cfca3794fcf1d4e02f4acf27435b159aaef
foxmon <jjockrod@naver.com>
2022-10-30 01:21:06 +0900

    Feat: ErrorComponent

---
+++ src/components/errorBoundary/errorComponent/ErrorComponent.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import Theme from "../../../util/theme";
+
+const ErrorComponent = () => {
+  return (
+    <ErrorComponentContainer>
+      <ErrorComponentImageWrapper>
+        <ErrorComponentImage src="../../../src/assets/images/logo-bg.png" />
+      </ErrorComponentImageWrapper>
+      <ErrorComponentWrapper>
+        <ErrorComponentTitle>
+          예상치 못한 오류가 발생했습니다 !
+        </ErrorComponentTitle>
+        <ErrorComponentLink href="/">GO BACK</ErrorComponentLink>
+      </ErrorComponentWrapper>
+    </ErrorComponentContainer>
+  );
+};
+
+const ErrorComponentContainer = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const ErrorComponentImageWrapper = styled.div`
+  width: auto;
+  margin-top: 1rem;
+`;
+
+const ErrorComponentImage = styled.img`
+  width: 620px;
+`;
+
+const ErrorComponentWrapper = styled.div`
+  width: auto;
+`;
+
+const ErrorComponentTitle = styled.p`
+  font-size: ${Theme.fontSize.moreBig};
+  font-weight: ${Theme.fontWeight.bold};
+  margin-top: -3rem;
+`;
+
+const ErrorComponentLink = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  cursor: pointer;
+  color: ${Theme.colors.black};
+  font-size: ${Theme.fontSize.littleBig};
+  font-weight: ${Theme.fontWeight.bold};
+  border-bottom: 3px solid ${Theme.colors.black};
+  transition: 0.5s;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export default ErrorComponent;
