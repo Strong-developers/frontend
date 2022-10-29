4edfe890a2f9576b806a8223a8feed4227366a3e
foxmon <jjockrod@naver.com>
2022-10-29 20:12:30 +0900

    Fix: AuthMemberSelector

--- src/components/auth/AuthMemberSelector.tsx
+++ src/components/auth/AuthMemberSelector.tsx
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 
 const AuthMemberSelector = () => {
-  const memberSelection = ["일반사용자", "보호소 Owner"];
+  const memberSelection = ["Volunteer", "Shelter"];
 
   return (
     <AuthMemberSelectorContainer>
-      {memberSelection.map((data, index) => {
-        return <li key={index}>{data}</li>;
-      })}
+      {memberSelection.map((data, index) => (
+        <AuthMemberSeletor key={index}>{data}</AuthMemberSeletor>
+      ))}
     </AuthMemberSelectorContainer>
   );
 };
 
-export default AuthMemberSelector;
+const AuthMemberSelectorContainer = styled.ul`
+  width: 100%;
+  display: flex;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  text-align: center;
+  margin-bottom: 1rem;
+`;
 
-const AuthMemberSelectorContainer = styled.ul``;
+const AuthMemberSeletor = styled.li`
+  width: 100%;
+  display: block;
+  cursor: pointer;
+`;
+
+export default AuthMemberSelector;
