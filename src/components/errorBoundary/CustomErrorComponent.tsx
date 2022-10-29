04750c8cb65f5aef53bb7ffd938bd9dbd1d242bd
foxmon <jjockrod@naver.com>
2022-10-29 20:42:16 +0900

    Fix: CustomErrorComponent

--- src/components/errorBoundary/CustomErrorComponent.tsx
+++ src/components/errorBoundary/CustomErrorComponent.tsx
@@ -4,7 +4,7 @@ import { FallbackProps } from "react-error-boundary";
 const CustomErrorComponent = ({ error }: FallbackProps) => {
   return (
     <React.Fragment>
-      {error instanceof Error && <div>Error!</div>}
+      {error instanceof Error && <a href="/">Go home</a>}
     </React.Fragment>
   );
 };
