2775352e48d4d7010f854d2b5e8e450b4db95c36
foxmon <jjockrod@naver.com>
2022-10-30 13:13:08 +0900

    Feat: AuthCommonSidebar

--- src/components/auth/common/AuthCommonSidebar.tsx
+++ src/components/auth/common/AuthCommonSidebar.tsx
@@ -4,17 +4,41 @@ import Theme from "../../../util/theme";
 const AuthCommonSidebar = () => {
   return (
     <AuthCommonSidebarContainer>
-      <div>사이드바</div>
+      <AuthCommonSidebarUsername>
+        안녕하세요. <strong>FoxMon !</strong>
+      </AuthCommonSidebarUsername>
+      {Array.from({ length: 5 })
+        .fill(0)
+        .map((_, idx) => (
+          <AuthCommonSidebarItemWrapper key={idx}>
+            <AuthCommonSidebarItem>사이드바</AuthCommonSidebarItem>
+          </AuthCommonSidebarItemWrapper>
+        ))}
     </AuthCommonSidebarContainer>
   );
 };
 
 const AuthCommonSidebarContainer = styled.div`
+  width: 11rem;
   height: 100%;
-  padding: 1rem;
+  padding: 1rem 1.25rem;
   display: flex;
   flex-direction: column;
-  background-color: ${Theme.colors.white};
+  font-size: ${Theme.fontSize.default};
+`;
+
+const AuthCommonSidebarUsername = styled.p`
+  font-size: ${Theme.fontSize.littleMoreBig};
+`;
+
+const AuthCommonSidebarItemWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const AuthCommonSidebarItem = styled.span`
+  display: inline-block;
+  margin: 0 auto;
+  cursor: pointer;
 `;
 
 export default AuthCommonSidebar;
