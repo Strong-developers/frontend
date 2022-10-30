f8713cc843571487a5dd79744c165e36cc6269df
foxmon <jjockrod@naver.com>
2022-10-30 12:28:40 +0900

    Fix: CustomRouter add fallback component

--- src/router/CustomRouter.tsx
+++ src/router/CustomRouter.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import HeaderLayout from "../components/common/headerLayout";
+import LoadingSpinner from "../components/spinner/LoadingSpinner";
 
 const MainComponentPage = React.lazy(() => import("../pages/Main"));
 const LoginComponentPage = React.lazy(() => import("../pages/Login"));
@@ -15,7 +16,7 @@ const ReservationComponentPage = React.lazy(
 const CustomRouter = () => {
   return (
     <React.Fragment>
-      <Suspense fallback={<div>Loading</div>}>
+      <Suspense fallback={<LoadingSpinner />}>
         <HeaderLayout />
         <Routes>
           <Route path="/" element={<MainComponentPage />} />
