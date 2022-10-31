e2779eff69bd91b2a5c2b8e83446bb5b9f2aeed9
origin1508 <origin1508@gmail.com>
2022-10-31 22:11:48 +0900

    Fix: icon 추가

--- src/components/icons/IconCreator.tsx
+++ src/components/icons/IconCreator.tsx
@@ -1,4 +1,7 @@
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
+import { IoMdTime } from "react-icons/io";
+import { IoCalendarNumberOutline } from "react-icons/io5";
 
 export function createNextButton(size: number, color: string) {
   return <BsArrowRight size={size} color={color} />;
@@ -7,3 +10,23 @@ export function createNextButton(size: number, color: string) {
 export function createPrevButton(size: number, color: string) {
   return <BsArrowLeft size={size} color={color} />;
 }
+
+export function createBackButton(size: number, color?: string) {
+  return <MdArrowBackIosNew size={size} color={color} style={commonStyle} />;
+}
+
+export function createForwardButton(size: number, color?: string) {
+  return <MdArrowForwardIos size={size} color={color} style={commonStyle} />;
+}
+
+export function createCalendarIcon(size: number, color?: string) {
+  return (
+    <IoCalendarNumberOutline size={size} color={color} style={commonStyle} />
+  );
+}
+
+export function createTimeIcon(size: number, color?: string) {
+  return <IoMdTime size={size} color={color} style={commonStyle} />;
+}
+
+const commonStyle = { display: "inline-block", verticalAlign: "middle" };
