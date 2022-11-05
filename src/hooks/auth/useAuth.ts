6b6580495d8d017fdbd633bc47d8375b4234877b
foxmon <jjockrod@naver.com>
2022-11-05 17:04:49 +0900

    Fix: useAuth

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,21 +1,22 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { authRegisterRequest, authLoginRequest } from "../../api/authFetcher";
 import { AuthFormType } from "../../types/auth/authType";
+import { setItem } from "../../storage/customStorage";
 
 const useAuth = () => {
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleRegisterSubmit = async (data: AuthFormType) => {
     try {
-      console.log(data.email);
-      const res = await authRegisterRequest("/auth/register", {
+      const { result } = await authRegisterRequest("/auth/register", {
         email: data.email,
         password: data.password,
         nickname: data.nickname,
         role: 1,
       });
+      if (result.id) window.location.replace("/login");
     } catch (error) {
-      throw new Error("request Error");
+      throw new Error("This is register request Error");
     }
   };
 
@@ -25,10 +26,12 @@ const useAuth = () => {
         email: data.email,
         password: data.password,
       });
-      console.log(result.token);
+      if (result.token) {
+        setItem(result.token);
+        window.location.replace("/");
+      }
     } catch (error) {
-      console.log(error);
-      throw new Error("request Error");
+      throw new Error("This is login request Error");
     }
   };
 
