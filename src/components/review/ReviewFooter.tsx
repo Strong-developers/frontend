94559ab58cf7179ec54b21b1bc732353ae3118a0
foxmon <jjockrod@naver.com>
2022-11-09 22:35:11 +0900

    Feat: ReviewFooter

---
+++ src/components/review/ReviewFooter.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import Theme from "../../util/theme";
+
+const ReviewFooter = () => {
+  return (
+    <ReviewFooterContainer>
+      <ReviewUserContainer>
+        <ReviewFooterUserImg src="../../src/assets/images/main-bg-03.jpg" />
+        <ReviewUserTextContainer>
+          <ReviewUserNickname>FoxMon</ReviewUserNickname>
+          <ReviewUserCity>의정부시</ReviewUserCity>
+        </ReviewUserTextContainer>
+      </ReviewUserContainer>
+    </ReviewFooterContainer>
+  );
+};
+
+const ReviewFooterContainer = styled.div`
+  width: 100%;
+`;
+
+const ReviewUserContainer = styled.div`
+  width: 100%;
+`;
+
+const ReviewFooterUserImg = styled.img`
+  display: inline-block;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  overflow: hidden;
+  vertical-align: middle;
+`;
+
+const ReviewUserTextContainer = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const ReviewUserNickname = styled.span`
+  display: block;
+  padding: 0 0.875rem;
+  font-size: ${Theme.fontSize.littleMoreBig};
+`;
+
+const ReviewUserCity = styled.span`
+  display: block;
+  padding: 0 0.875rem;
+  font-size: ${Theme.fontSize.small};
+`;
+
+export default ReviewFooter;
