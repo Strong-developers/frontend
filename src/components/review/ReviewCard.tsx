3b8f358a7aced5468e36bacfeaba882cadc62e44
foxmon <jjockrod@naver.com>
2022-11-09 22:35:30 +0900

    Feat: ReviewCard

---
+++ src/components/review/ReviewCard.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import ReviewHeader from "./ReviewHeader";
+import ReviewBody from "./ReviewBody";
+import ReviewFooter from "./ReviewFooter";
+import Theme from "../../util/theme";
+
+const ReviewCard = () => {
+  return (
+    <ReviewCardContainer>
+      <ReviewHeader />
+      <ReviewBody />
+      <ReviewFooter />
+    </ReviewCardContainer>
+  );
+};
+
+const ReviewCardContainer = styled.div`
+  width: 18rem;
+  padding: 1rem;
+  border-radius: 8px;
+  background-color: ${Theme.colors.white};
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.16);
+  cursor: pointer;
+  transition: 0.4s;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export default ReviewCard;
