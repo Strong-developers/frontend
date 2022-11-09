14e3aad70cbb2c3f0c63a35c460c6c6da097173f
foxmon <jjockrod@naver.com>
2022-11-10 00:52:07 +0900

    Fix: ReviewBody

--- src/components/review/ReviewBody.tsx
+++ src/components/review/ReviewBody.tsx
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 import Theme from "../../util/theme";
 
-// const MAX_STRING_LENGTH = 60
-// TODO: 추후에 넘어온 TEXT의 길이가 60을 넘을 경우 ...으로 최대 길이 제한 걸어두기
+interface ReviewBodyProps {
+  description: string;
+}
 
-const ReviewBody = () => {
+const ReviewBody = ({ description }: ReviewBodyProps) => {
   return (
     <ReviewBodyContainer>
-      <ReviewBodyText>
-        이 보호소에서 봉사를 해봤는데요. 너무 좋았어요. 또 하고 싶고 가족이나
-        친구를 데리고 와서 하고싶어요!!
-      </ReviewBodyText>
+      <ReviewBodyText>{description}</ReviewBodyText>
     </ReviewBodyContainer>
   );
 };
@@ -26,6 +24,8 @@ const ReviewBodyText = styled.p`
   font-size: ${Theme.fontSize.small};
   padding: 1rem 0.875rem;
   border-bottom: 2px solid ${Theme.colors.moreTp};
+  text-overflow: ellipsis;
+  overflow: hidden;
 `;
 
 export default ReviewBody;
