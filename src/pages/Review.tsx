bee158066ae6d84965746414579d0a83f68fa3a2
origin1508 <origin1508@gmail.com>
2022-11-11 23:10:14 +0900

    Fix: Review 수정

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -2,34 +2,42 @@ import styled from "styled-components";
 import useReview from "../hooks/review/useReview";
 import ReviewContainer from "../components/review/ReviewContainer";
 import Pagination from "../components/pagination/Pagination";
+import CommonBasePageComponent from "../components/hoc/CommonBasePageComponent";
 import Theme from "../util/theme";
 
 const Review = () => {
   const { reviews, totalPages, curPage, setCurPage } = useReview();
 
   return (
-    <ReviewWrapper>
-      <ReviewTitleContainer>
-        <ReviewText>후기 게시판</ReviewText>
-      </ReviewTitleContainer>
-      <ReviewContainer reviews={reviews} />
-      <Pagination
-        totalPages={totalPages}
-        curPage={curPage}
-        setCurPage={setCurPage}
-      />
-    </ReviewWrapper>
+    <CommonBasePageComponent>
+      <ReviewWrapper>
+        <ReviewTitleContainer>
+          <ReviewText>후기 게시판</ReviewText>
+        </ReviewTitleContainer>
+        <ReviewContainer reviews={reviews} />
+        <Pagination
+          totalPages={totalPages}
+          curPage={curPage}
+          setCurPage={setCurPage}
+        />
+      </ReviewWrapper>
+    </CommonBasePageComponent>
   );
 };
 
 const ReviewWrapper = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  width: 70%;
+  height: 100%;
 `;
 
 const ReviewTitleContainer = styled.div`
-  padding-top: 3rem;
-  padding-bottom: 1rem;
-  padding-left: 4rem;
+  display: flex;
+  align-items: center;
+  height: 5rem;
 `;
 
 const ReviewText = styled.p`
