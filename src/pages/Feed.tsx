58a153392a057ea40c7d759cba267f886dcc592d
origin1508 <origin1508@gmail.com>
2022-12-22 20:43:08 +0900

    fix: ReservationModal 제거

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,24 +1,16 @@
-import React from "react";
 import styled from "styled-components";
 import FeedHeader from "../components/feed/FeedHeader";
 import FeedBody from "../components/feed/FeedBody";
-import ReservationModal from "../components/reservation/modal/ReservationModal";
-import useCustomModal from "../hooks/useCustomModal";
 import Theme from "../util/theme";
 import CommonBasePageComponent from "../components/hoc/CommonBasePageComponent";
 
 const Feed = () => {
-  const [isModalOpen, handleModalState] = useCustomModal();
   return (
     <CommonBasePageComponent>
       <FeedContainer>
-        <FeedHeader onReservationModalOpenEvent={handleModalState} />
+        <FeedHeader />
         <FeedBody />
       </FeedContainer>
-      <ReservationModal
-        isModalOpen={isModalOpen}
-        onCloseButtonClickEvent={handleModalState}
-      />
     </CommonBasePageComponent>
   );
 };
