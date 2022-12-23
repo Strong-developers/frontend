5784aa8d01a8481735cb62d381a23be6df5dacb0
origin1508 <origin1508@gmail.com>
2022-12-23 14:18:04 +0900

    fix: authFetcher 수정정

--- src/api/authFetcher.ts
+++ src/api/authFetcher.ts
@@ -1,12 +1,12 @@
 import customAxios from "../util/customAxios";
-import { AuthFormType } from "../types/auth/authType";
+import { AuthForm } from "../types/auth/authType";
 
-export async function authLoginRequest(url: string, data: AuthFormType) {
+export async function authLoginRequest(url: string, data: AuthForm) {
   const res = await customAxios.post(url, data);
   return res.data;
 }
 
-export async function authRegisterRequest(url: string, data: AuthFormType) {
+export async function authRegisterRequest(url: string, data: AuthForm) {
   const res = await customAxios.post(url, data);
   return res.data;
 }
