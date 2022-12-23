4f4585bcab00a3ff0289e66ef3e49112f165cae3
origin1508 <origin1508@gmail.com>
2022-12-23 14:16:56 +0900

    fix: Login 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,28 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import useAuth from "../hooks/auth/useAuth";
+import { yupResolver } from "@hookform/resolvers/yup";
 import CommonBasePageComponent from "../components/hoc/CommonBasePageComponent";
 import AuthLogin from "../components/auth/AuthLogin";
-import { AuthFormType } from "../types/auth/authType";
+import { AuthForm } from "../types/auth/authType";
 
 const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<AuthFormType>({
+  const { loginSchema, handleLoginSubmit } = useAuth();
+  const methods = useForm<AuthForm>({
     mode: "onChange",
     defaultValues: {
       email: "",
       password: "",
     },
+    resolver: yupResolver(loginSchema),
   });
-  const { handleLoginSubmit } = useAuth();
 
   return (
     <CommonBasePageComponent>
-      <AuthLogin
-        register={register}
-        errors={errors}
-        onLoginSubmitEvent={handleSubmit(handleLoginSubmit)}
-      />
+      <FormProvider {...methods}>
+        <AuthLogin
+          onLoginSubmitEvent={methods.handleSubmit(handleLoginSubmit)}
+        />
+      </FormProvider>
     </CommonBasePageComponent>
   );
 };
