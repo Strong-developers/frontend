14d1c6014fa775bc1f5baa4fb71cc447a68734db
foxmon <jjockrod@naver.com>
2022-12-23 22:30:55 +0900

    Fix: feedFetcher

--- src/api/feedFetcher.ts
+++ src/api/feedFetcher.ts
@@ -10,3 +10,46 @@ export async function feedListRequest(id: string, page: number) {
   const { result } = res.data;
   return result;
 }
+
+export async function feedPostRequest(id: string, description: string) {
+  const res = await customAxios.post(
+    `/shelter/${id}`,
+    {
+      description: description,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${getItem()}`,
+      },
+    }
+  );
+  return res.data;
+}
+
+export async function feedUpdateRequest(
+  id: string,
+  postId: string,
+  description: string
+) {
+  const res = await customAxios.put(
+    `/shelter/${id}?postId=${postId}`,
+    {
+      description: description,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${getItem()}`,
+      },
+    }
+  );
+  return res.data;
+}
+
+export async function feedDeleteRequest(id: string, postId: string) {
+  const res = await customAxios.delete(`/shelter/${id}?postId=${postId}`, {
+    headers: {
+      Authorization: `Bearer ${getItem()}`,
+    },
+  });
+  return res.data;
+}
