5a1521d587ed5b13a3784451b4290d70497712c3
origin1508 <origin1508@gmail.com>
2022-12-23 14:09:01 +0900

    fix: AuthForm 수정, Props타입들 추가

--- src/types/auth/authType.ts
+++ src/types/auth/authType.ts
@@ -1,17 +1,42 @@
-import React from "react";
-import { UseFormRegister, FieldErrorsImpl } from "react-hook-form";
+import { FormEvent, Dispatch, SetStateAction } from "react";
 
-export interface AuthFormType {
+export interface AuthForm {
   email: string;
   nickname?: string;
   password: string;
   confirmPassword?: string;
   role?: number;
+  name?: string;
+  region?: string;
+  phoneNumber?: string;
+  description?: string;
+  caution?: string;
 }
 
-export interface AuthFormPropsType {
-  register: UseFormRegister<AuthFormType>;
-  errors: Partial<FieldErrorsImpl<AuthFormType>>;
-  onRegisterSubmitEvent?: (e: React.FormEvent) => void;
-  onLoginSubmitEvent?: (e: React.FormEvent) => void;
+export interface AuthFormProps {
+  onRegisterSubmitEvent?: (e: FormEvent) => void;
+  onLoginSubmitEvent?: (e: FormEvent) => void;
+}
+
+export interface AuthRegisterProps {
+  selectedRole: number;
+  setSelectedRole: Dispatch<SetStateAction<number>>;
+  onRegisterSubmitEvent?: (e: FormEvent) => void;
+}
+
+export interface AuthRoleSelectorProps {
+  selectedRole: number;
+  setSelectedRole: Dispatch<SetStateAction<number>>;
+  resetPosition: () => void;
+}
+
+export interface AuthRegisterButtonsProps {
+  selectedRole: number;
+  currentPosition: number;
+  onNextButtonClick: () => void;
+  onPrevButtonClick: () => void;
+}
+
+export interface AuthRegisterForms {
+  [key: number]: JSX.Element;
 }
