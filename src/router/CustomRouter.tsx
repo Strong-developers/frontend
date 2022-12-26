e282e654579194fc01d4dcbe12fba6bd5d27695f
origin1508 <origin1508@gmail.com>
2022-12-26 15:43:15 +0900

    fix: import 수정, 예약페이지 route 추가

--- src/router/CustomRouter.tsx
+++ src/router/CustomRouter.tsx
@@ -1,26 +1,27 @@
-import React, { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "../components/common/Header";
 import LoadingSpinner from "../components/spinner/LoadingSpinner";
 
-const MainComponentPage = React.lazy(() => import("../pages/Main"));
-const LoginComponentPage = React.lazy(() => import("../pages/Login"));
-const LogoutComponentPage = React.lazy(() => import("../pages/Logout"));
-const RegisterComponentPage = React.lazy(() => import("../pages/Register"));
-const CalendarComponentPage = React.lazy(
+const MainComponentPage = lazy(() => import("../pages/Main"));
+const LoginComponentPage = lazy(() => import("../pages/Login"));
+const LogoutComponentPage = lazy(() => import("../pages/Logout"));
+const RegisterComponentPage = lazy(() => import("../pages/Register"));
+const CalendarComponentPage = lazy(
   () => import("../components/calendar/Calendar")
 );
-const ReservationComponentPage = React.lazy(
-  () => import("../pages/Reservation")
+const ReservationComponentPage = lazy(() => import("../pages/Reservation"));
+const FeedComponentPage = lazy(() => import("../pages/Feed"));
+const MyInformationComponentPage = lazy(() => import("../pages/MyPage"));
+const ChatComponentPage = lazy(() => import("../pages/Chat"));
+const ReviewComponentPage = lazy(() => import("../pages/Review"));
+const UserReservationComponentPage = lazy(
+  () => import("../pages/UserReservation")
 );
-const FeedComponentPage = React.lazy(() => import("../pages/Feed"));
-const MyInformationComponentPage = React.lazy(() => import("../pages/MyPage"));
-const ChatComponentPage = React.lazy(() => import("../pages/Chat"));
-const ReviewComponentPage = React.lazy(() => import("../pages/Review"));
 
 const CustomRouter = () => {
   return (
-    <React.Fragment>
+    <>
       <Suspense fallback={<LoadingSpinner />}>
         <Header />
         <Routes>
@@ -34,9 +35,13 @@ const CustomRouter = () => {
           <Route path="/mypage" element={<MyInformationComponentPage />} />
           <Route path="/chat" element={<ChatComponentPage />} />
           <Route path="/review" element={<ReviewComponentPage />} />
+          <Route
+            path="/reservation/:shelterId"
+            element={<UserReservationComponentPage />}
+          />
         </Routes>
       </Suspense>
-    </React.Fragment>
+    </>
   );
 };
 
